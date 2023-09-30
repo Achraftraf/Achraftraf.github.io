80bbf72999baac33f78cf54a7798a1b68df7cab1
Achraftraf <achrafzarouki20@gmail.com>
2023-09-30 19:30:12 +0100

    the icons and links

--- components/Nav.js
+++ components/Nav.js
@@ -31,11 +31,12 @@ import { useRouter } from 'next/router';
 const Nav = () => {
   const router = useRouter();
   const pathname = router.pathname;   
-  return <nav className='flex flex-col items-center xl:justify-center  gap-y-4 fixed  h-max  bottom-0 mt-auto xl:right-[2%] bg-red-500/30 p-20 '>
+  return <nav className='flex flex-col items-center xl:justify-center  gap-y-4 fixed  h-max  bottom-0 mt-auto xl:right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen'>
     {}
-    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-between gap-y-10 px-4 md:px-40 xl:px-0 h-80 bg-red-200/10 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
+    <div className='flex w-full xl:flex-col items-center justify-between xl:justify-between gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-red-200/10 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
       {navData.map((link , index) => {
-          return <Link href={link.path} key={index} >{link.icon}</Link>
+          return <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300 `} href={link.path} key={index} >
+            <div>{link.icon}</div></Link>
       }
       )} 
     </div>
