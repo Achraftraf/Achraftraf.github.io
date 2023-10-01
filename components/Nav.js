1046afc490d669e2c11dffff3f5c753d03421842
Achraftraf <achrafzarouki20@gmail.com>
2023-10-01 01:07:49 +0100

    add names to links

--- components/Nav.js
+++ components/Nav.js
@@ -36,6 +36,16 @@ const Nav = () => {
     <div className='flex w-full xl:flex-col items-center justify-between xl:justify-between gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] bg-red-200/10 xl:h-max py-8 bg-white/10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full'>
       {navData.map((link , index) => {
           return <Link className={`${link.path === pathname && 'text-accent'} relative flex items-center group hover:text-accent transition-all duration-300 `} href={link.path} key={index} >
+          {}
+          <div className='absolute pr-14 right-0 hidden xl:group-hover:flex'>
+            <div className='bg-white relative flex text-primary itmes-center p-[6px] rounded-[3px]'>
+<div className='text-[12px] leading-none font-semibold capitalize'>{link.name}</div>
+{}
+<div className='border-solid border-l-white border-l-8 border-y-transparent border-y-[6px] border-r-0 absolute -right-2'></div>
+</div>
+</div>
+          {}
+
             <div>{link.icon}</div></Link>
       }
       )} 
