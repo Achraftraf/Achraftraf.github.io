bfd9e73f53443b7712f5d8620380580ce5a3e3d3
Achraftraf <achrafzarouki20@gmail.com>
2023-10-02 00:44:56 +0100

    edit the avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,39 +1,9 @@
-import React, { useState, useEffect } from "react";
 import Image from "next/image";
 
 const Avatar = () => {
-  const [currentImage, setCurrentImage] = useState("/av1-removebg.png");
-  const imageList = [
-    "/av1-removebg.png",
-    "/achrafzarouki-removebg.png",
-    "/av2-removebg.png", // Add your image paths here
-  ]; 
-
-  useEffect(() => {
-    // Use setInterval to switch the image every 5 seconds (5000 milliseconds)
-    const interval = setInterval(() => {
-      const currentIndex = imageList.indexOf(currentImage);
-      const nextIndex = (currentIndex + 1) % imageList.length;
-      setCurrentImage(imageList[nextIndex]);
-    }, 800);
-
-    // Clear the interval when the component unmounts
-    return () => {
-      clearInterval(interval);
-    };
-  }, [currentImage]); // useEffect dependency on currentImage
-
-  return (
-    <div className="hidden xl:flex xl:max-w-none">
-      <Image
-        src={currentImage}
-        width={737}
-        height={678}
-        alt=""
-        className="translate-z-0 w-full h-full"
-      />
-    </div>
-  );
+  return <div className="hidden xl:flex xl:max-w-none">
+    <Image src={'/av-removebg.png'}  width={737} height={678} alt="" className='translate-z-0 w-full h-full' />
+  </div>;
 };
 
-export default Avatar;
+export default Avatar;
--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,7 @@ initial="hidden" animate="show" exit="hidden" className="hidden xl:flex"
       <div>Particles</div>
       {}
       <motion.div variants={fadeIn('up',0.5)}
-initial="hidden" animate="show" exit="hidden" transition={{duration: 1,ease: 'easeInOut'}} className='w-full h-full max-w-[737px] max-h-[678px] absolute  -bottom-30 lg:bottom-300 lg:right-[-1%]'>
+initial="hidden" animate="show" exit="hidden" transition={{duration: 1,ease: 'easeInOut'}} className='w-full h-full max-w-[737px] max-h-[678px] absolute  -bottom-30 lg:bottom-300 lg:right-[5%]'>
       <Avatar />
     </motion.div>
     </div>  
