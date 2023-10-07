1fa2feec44ff8da3308297347afbffd6b2d72599
Achraftraf <achrafzarouki20@gmail.com>
2023-10-07 09:37:03 +0100

    add the animations

--- components/ParticlesContainer.js
+++ components/ParticlesContainer.js
@@ -1,5 +1,96 @@
+import React, { useCallback } from 'react';
+import { Particles } from 'react-tsparticles';
+import { loadFull } from 'tsparticles';
+
 const ParticlesContainer = () => {
-  return <div>Particles Container</div>;
+  // init
+  const particlesInit = useCallback(async (engine) => {
+    await loadFull(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async () => {
+    // You can perform additional actions here when particles are loaded
+  }, []);
+
+  return (
+    <Particles
+      className='w-full h-full absolute translate-z-0'
+      id='tsparticles'
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={{
+        fullScreen: { enable: false },
+        background: {
+          color: {
+            value: 'black' // Specify a color value here, e.g., 'black'
+          }
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: false,
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+            resize: true,
+          },
+          modes: {
+            push: {
+              quantity: 90,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#e68e2e',
+          },
+          links: {
+            color: '#f5d393',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          collisions: {
+            enable: true,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'bounce',
+            random: false,
+            speed: 1,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+              area: 800,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {type: 'circle',},
+          size: {
+            value: {
+              min: 1,
+              max: 3 // You need to specify both min and max values
+            },
+          },
+detectRetina : true,
+        },
+      }}
+    />
+  );
 };
 
 export default ParticlesContainer;
--- pages/index.js
+++ pages/index.js
@@ -38,7 +38,7 @@ initial="hidden" animate="show" exit="hidden" className="hidden xl:flex"
       {}
       <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
       {}
-      <div>Particles</div>
+      <ParticlesContainer />
       {}
       <motion.div variants={fadeIn('up',0.5)}
 initial="hidden" animate="show" exit="hidden" transition={{duration: 1,ease: 'easeInOut'}} className='w-full h-full max-w-[737px] max-h-[678px] absolute  -bottom-30 lg:bottom-300 lg:right-[5%]'>
