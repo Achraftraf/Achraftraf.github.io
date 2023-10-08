16e36f3cc038b7f61f7d3d7f44085d78ad1000eb
Achraftraf <achrafzarouki20@gmail.com>
2023-10-08 14:42:21 +0100

    i add the name and links

--- components/Avatar.js
+++ components/Avatar.js
@@ -2,8 +2,10 @@ import Image from "next/image";
 
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
-    <Image src={'/av-removebg.png'}  width={737} height={678} alt="" className='translate-z-0 w-full h-full' />
+<Image src={'/av-removebg.png'}  width={737} height={678} alt="" className='translate-z-0 w-full h-full' />
   </div>;
 };
 
 export default Avatar;
+
+// 
--- components/Socials.js
+++ components/Socials.js
@@ -1,5 +1,25 @@
+import Link from "next/link";
+
+
+import {
+  RiYoutubeLine,
+  RiInstagramLine,
+  RiFacebookLine,
+  RiGithubLine,
+  RiMailLine,
+  RiLinkedinLine,
+  } from 'react-icons/ri';
+
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className="flex items-center gap-x-5 text-lg">
+    <Link href={''} className='hover:text-accent transition-all duration-300'><RiYoutubeLine /></Link>
+    <Link href={'https://github.com/Achraftraf/Achraftraf.github.io'} className='hover:text-accent transition-all duration-300'><RiGithubLine /></Link>
+    <Link href={'https://www.linkedin.com/in/achraf-zarouki-058888244/'} className='hover:text-accent transition-all duration-300'><RiLinkedinLine /></Link>
+    <Link href={''} className='hover:text-accent transition-all duration-300'><RiInstagramLine /></Link>
+    <Link href={''} className='hover:text-accent transition-all duration-300'><RiFacebookLine /></Link>
+    <Link href={''} className='hover:text-accent transition-all duration-300'><RiMailLine /></Link>
+
+  </div>;
 };
 
 export default Socials;
