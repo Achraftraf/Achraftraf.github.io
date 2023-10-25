19ba31d5d4206f864666ec4920c2d6cf238cf3f2
Achraftraf <achrafzarouki20@gmail.com>
2023-10-25 23:42:08 +0100

    add the avatar image

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 const Avatar = () => {
   return <div className="hidden xl:flex xl:max-w-none">
 <Image src={'/av-removebg.png'}  width={737} height={678} alt="" className='translate-z-0 w-full h-full' />
+
   </div>;
 };
 
