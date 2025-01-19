6382e9cec4073e8e5f9cef30f19ca097072bca3c
Achraftraf <achrafzarouki20@gmail.com>
2025-01-19 17:48:20 +0100

    update the readme file including the chat bot description

--- pages/index.js
+++ pages/index.js
@@ -52,8 +52,7 @@ const Home = () => {
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {}
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
-        {}
-        <ParticlesContainer />
+        {} <ParticlesContainer />
         {}
         <motion.div
           variants={fadeIn("up", 0.5)}
