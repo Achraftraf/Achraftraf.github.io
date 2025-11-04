8ff6c59ed047ea71c6fd2f908d54021225b44d1d
Achraftraf <achrafzarouki20@gmail.com>
2025-11-04 15:51:42 +0100

    the config of next js to fix all the problems also i fix the names to test it

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ const nextConfig = {
   swcMinify: true,
   eslint: {
     ignoreDuringBuilds: true,
-},
+  },
+  output: 'export', // For static site deployment on Netlify
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  trailingSlash: true, // Helps with static hosting
 }
 
 module.exports = nextConfig
--- tailwind.config.js
+++ tailwind.config.js
@@ -27,8 +27,8 @@ module.exports = {
         accent: '#F13024',
       },
       backgroundImage: {
-        explosion: 'url("/bg-explosion.png")',
-        circles: 'url("/bg-circles.png")',
+        explosion: 'url("/bg-explosion.png")',  
+        circles: 'url("/circles.png")',
         circleStar: 'url("/circle-star.svg")',
         site: 'url("/site-bg.svg")',
       },
